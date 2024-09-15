26a657fc56c18d8cafbf44809419b7d4b846e9b9
Kai Schulz <info@sparrowmedia.de>
2024-09-15 11:50:13 +0200

    defined data structure

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 // Firebase References
 let baseURL = 'https://solo-join-default-rtdb.europe-west1.firebasedatabase.app/'
-let testdiv = document.getElementById('testdiv')
 const testInputValue = document.getElementById('test');
 let database = [];
 
-function push() {
+/**
+ * Initializes Database Logic
+ */
+async function init() {
+    let data = await getDataFromFirebase('/test');
+    let dataIds = Object.keys(data);
+
+    for (let index = 0; index < dataIds.length; index++) {
+        database.push ({
+            objectId: dataIds[index],
+            data: data[dataIds[index]]
+        })
+    };
+
+    console.log('Database:', database)
+}
+
+/**
+ * Loads Firebase Realtime DB
+ */
+async function getDataFromFirebase(path="") {
+    let response = await fetch(baseURL + path + '.json')
+    return responseToJson = await response.json();
+}
+
+/**
+ * Gets Input Data
+ */
+function getInputData() {
     data = testInputValue.value;
     pushDataToFirebase('/test', data);
 }
 
+/**
+ * Push Input Values To Firebase Realtime DB
+ */
 async function pushDataToFirebase(path="", data) {
     try { 
         await fetch(baseURL + path + '.json', {
@@ -21,15 +51,4 @@ async function pushDataToFirebase(path="", data) {
     catch(error) {
         console.log ('Error Brudi');
     }
-}
-
-async function getDataFromFirebase(path) {
-    let response = await fetch(baseURL + path + '.json')
-    let responseToJson = await response.json();
-
-
-    database = responseToJson;
-    console.log(database);
-    let test = Object.entries(database)
-    console.log(test);
 }
