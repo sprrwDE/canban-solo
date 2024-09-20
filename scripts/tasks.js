1528466a0e2b1e6be67fbf23dd429d1c7de9e739
Kai Schulz <info@sparrowmedia.de>
2024-09-20 08:45:19 +0200

    added subtask logic on task form

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -6,38 +6,24 @@ let headlineRef = document.getElementById('headline');
 let textRef = document.getElementById('text');
 let subRef = document.getElementById('sub-input');
 
-/**
- * Gets Input Field Data and Stores Data in Object
- */
-function getInputData(event) {
-    event.preventDefault()
-    let status = document.getElementById('status');
-    let assignedTo = document.querySelector('input[name="assign-contact"]:checked');
-    input = {
-        headline: headlineRef.value,
-        text: textRef.value,
-        status: status.value,
-        assigned: assignedTo.value,
-        subtask: ''
-    }
-    pushDataToFirebase('tasks', input);
-    resetInputFields();
-}
-
-///////////////////////
-
+// Subtask References
 let subFormRef = document.getElementById('sub-form')
 let subInputRef = document.getElementById('sub-input');
 let subtasks = [];
 
+/**
+* Pushes Subtask Input Value into Array
+*/
 function addSubtask(event) {
     event.preventDefault();
     subtasks.push(subInputRef.value)
-    console.log(subtasks)
     renderSubtasks();
     subInputRef.value = '';
 }
 
+/**
+* Renders Subtasks into Task Form
+*/
 function renderSubtasks() {
     subFormRef.innerHTML = '';
     for (let i = 0; i < subtasks.length; i++) {
@@ -45,15 +31,43 @@ function renderSubtasks() {
     }
 }
 
+/**
+* Deletes Subtask in Subtask Array
+*/
 function deleteSubtask(i) {
     subtasks.splice([i], 1);
     renderSubtasks();
 }
 
-// Subtask In Objekt Umwaldeln und an input übergeben
+/**
+* Transforms Subtask Array into Object 
+*/
+function getSubtaskObject() {
+    const subtaskObject = subtasks.reduce((result, subtask, index) => {
+        result[index] = subtask;
+        return result;
+    }, {})
+    return subtaskObject;
+}
 
-
-
+/**
+ * Gets Complete Input Field Data and Stores Data in Object
+ */
+function getInputData(event) {
+    event.preventDefault()
+    let status = document.getElementById('status');
+    let assignedTo = document.querySelector('input[name="assign-contact"]:checked');
+    const subtaskObject = getSubtaskObject();
+    input = {
+        headline: headlineRef.value,
+        text: textRef.value,
+        status: status.value,
+        assigned: assignedTo.value,
+        subtask: subtaskObject
+    }
+    pushDataToFirebase('tasks', input);
+    resetInputFields();
+}
 
 /**
  * Resets Input Field Values
@@ -88,7 +102,7 @@ async function inititializeBoard() {
     urgentRef.innerHTML = '';
     setDatabase();
 }
-    
+
 /**
  * Defines the Database Array 
  */
