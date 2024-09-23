80f0ad317eed86979755e4c0771dee0069263b2c
Kai Schulz <info@sparrowmedia.de>
2024-09-23 16:45:52 +0200

    login works now

--- scripts/login.js
+++ scripts/login.js
@@ -1,19 +1,21 @@
 let loginScreenRef = document.getElementById('login');
 
+let userArray = []
+
 function logOut() {
     loginScreenRef.classList.remove('d-none')
 }
 
 function logIn(event) {
-    event.preventDefault()
+    if (event) { event.preventDefault() }
     loginScreenRef.classList.add('d-none')
 }
 
 async function initializeSignup() {
     let response = await getDataFromFirebase("user/");
     if (response) {
-        let userArray = Object.entries(response).map(([id, userData]) => ({ id, ...userData }));
-        console.log("response", userArray);
+        userArray = Object.entries(response).map(([id, userData]) => ({ id, ...userData }));
+        console.log("response", user);
     }
 }
 
@@ -21,21 +23,34 @@ function getSignupInputData(event) {
     event.preventDefault();
     let signupEmailRef = document.getElementById('signup-email');
     let signupPwRef = document.getElementById('signup-pw');
+
     let data = {
         email: signupEmailRef.value,
         pw: signupPwRef.value
     }
     console.log("eingabe", data);
-    pushDataToFirebase(path = "user/", data)
-
-    /**
-     * if (data.email == userArray.contains(email) && dara.pw == userArray.contains(pw) {
-     * login()} else {
-     * alert('falsche userdaten')})
-     */
+    pushDataToFirebase("user/", data)
+    signupEmailRef.value = '';
+    signupPwRef.value = '';
 }
+
+function contains(email, pw, event) {
+    let userFound = userArray.find(user => user.email === email && user.pw === pw);
+
+    if (userFound) {
+        alert('Erfolgreich eingeloggt!');
+        logIn(event);
+    } else {
+        alert('Falsche Userdaten');
+    }
+};
 
 function getLoginInputData(event) {
     let loginEmailRef = document.getElementById('login-email');
     let loginPwRef = document.getElementById('login-pw');
+    let email = loginEmailRef.value;
+    let pw = loginPwRef.value;
+    contains(email, pw, event)
+    loginEmailRef.value = '';
+    loginPwRef.value = '';
 }
